dbca8886d364711136f99c0eba8391c7a9783882
aaaahliyah <aaliyah.evans@utexas.edu>
2022-04-06 17:08:11 -0500

    Create AET319_Project2.ino

---
+++ AET319_Project2/AET319_Project2.ino
@@ -0,0 +1,12 @@
+// Media & Technology Project 2 Code
+// by Aaliyah and Krista
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
