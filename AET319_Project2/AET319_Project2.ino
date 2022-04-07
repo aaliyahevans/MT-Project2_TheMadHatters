39aa8ca83fd0344f15e2c1110bcd48a8b0f4a910
aaaahliyah <aaliyah.evans@utexas.edu>
2022-04-07 14:53:27 -0500

    created variables, initialized button pins

--- AET319_Project2/AET319_Project2.ino
+++ AET319_Project2/AET319_Project2.ino
@@ -1,9 +1,22 @@
-// Media & Technology Project 2 Code
-// by Aaliyah and Krista
+/*
+
+Alice in Wonderland Enclosure
+Team Name: The Mad Hatters
+Team Members: Aaliyah and Krista
+Description: Button and servo program
+*/
+
+// adding servo code library
+#include<Servo.h>;
+
+// VARIABLES
+Servo myServo;
+const int buttonPin = 7;
 
 void setup() {
   // put your setup code here, to run once:
-
+  myServo.attach(9);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
